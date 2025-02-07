425fc07f524cb3a2e8ffd5062b78158e329eb060
ing Wilnel <perezwilnel12@gmail.com>
2025-02-06 20:10:04 -0400

    Agregar archivos de proyecto.

---
+++ Data/UsuarioContext.cs
@@ -0,0 +1,21 @@
+﻿using Entidad;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Data
+{
+    public class UsuarioContext : DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder option) 
+        {
+
+            option.UseSqlServer("server= WILNELP12\\SQLEXPRESS; initial catalo= Usuario; " +
+                                "integrated securit= true; TrustServerCertificate= tru;");
+
+        }
+
+        public DbSet<Entidad> Usuario { get; set; }
+    }
+}
---
+++ Entidad/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace Entidad
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string CorreoElectronico { get; set;}
+        public string NombreCompleto { get; set; }
+        public string Carrera { get; set;}
+        public string Contrasena { get; set;}
+
+        
+
+    }
+}
